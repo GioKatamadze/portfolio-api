5a622a4ee64d3ed79853b65cdd73dcd0eb031006
Gio Katamadze <giokat.inbox@gmail.com>
2023-01-15 01:03:26 +0400

    update request data in mail option

--- src/server.js
+++ src/server.js
@@ -33,10 +33,10 @@ app.use("/api/sendemail", (req, res) => {
   });
 
   const mailOptions = {
-    from: req.body.data.email,
+    from: req.body.email,
     to: "giokat.inbox@gmail.com",
-    subject: `Message from ${req.body.data.name}: ${req.body.data.subject}`,
-    text: req.body.data.message,
+    subject: `Message from ${req.body.name}: ${req.body.subject}`,
+    text: req.body.message,
   };
 
   // Send Email
