23f67819c5a566b3354304d794e2065099e2a354
Gio Katamadze <giokat.inbox@gmail.com>
2023-01-16 21:50:27 +0400

    update nodemailer

--- src/SendEmail.js
+++ src/SendEmail.js
@@ -2,6 +2,7 @@ import nodemailer from "nodemailer";
 import dotenv from "dotenv";
 
 dotenv.config();
+require("dotenv").config();
 const email = `${process.env.EMAIL}`;
 const pass = `${process.env.PASS}`;
 
