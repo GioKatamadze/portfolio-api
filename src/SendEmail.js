1a9d59a89d96c1d79524936b323448aaba14f630
Gio Katamadze <giokat.inbox@gmail.com>
2023-01-16 19:59:14 +0400

    update nodemailer

--- src/SendEmail.js
+++ src/SendEmail.js
@@ -7,16 +7,16 @@ const sendEmail = async (req, res) => {
   const transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
-      user: process.env.EMAIL,
+      user: `"``${process.env.EMAIL}``"`,
       //   user: "giokat.inbox@gmail.com",
-      pass: process.env.PASS,
+      pass: `"``${process.env.PASS}``"`,
       //   pass: "byuepxrvcgryilwt",
     },
   });
 
   const mailOptions = {
     from: `${req.body.mailerState.email}`,
-    to: process.env.EMAIL,
+    to: `"``${process.env.EMAIL}``"`,
     // to: "giokat.inbox@gmail.com",
     subject: `Message from ${req.body.mailerState.name}: ${req.body.mailerState.subject}`,
     text: `${req.body.mailerState.message}`,
