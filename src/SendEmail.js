8a1d4bfcd4674348f149b3595a02c6682fe626bd
Gio Katamadze <giokat.inbox@gmail.com>
2023-01-16 20:36:43 +0400

    update nodemailer

--- src/SendEmail.js
+++ src/SendEmail.js
@@ -1,21 +1,20 @@
 import nodemailer from "nodemailer";
 import dotenv from "dotenv";
 
-dotenv.config();
-
 const sendEmail = async (req, res) => {
+  dotenv.config();
   const transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
-      user: "giokat.inbox@gmail.com",
-      pass: "byuepxrvcgryilwt",
+      user: `${process.env.EMAIL}`,
+      pass: `${process.env.PASS}`,
     },
   });
 
   const mailOptions = {
     from: "Giokatamadze.com",
-    to: "giokat.inbox@gmail.com",
-    subject: `Message from ${req.body.mailerState.emailValue} - ${req.body.mailerState.name}: ${req.body.mailerState.subject}`,
+    to: `${process.env.EMAIL}`,
+    subject: `Website message from - ${req.body.mailerState.emailValue} - ${req.body.mailerState.name}: ${req.body.mailerState.subject}`,
     text: `${req.body.mailerState.message}`,
   };
 
